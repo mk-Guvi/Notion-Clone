f21623433ced14d5163ff57a8fee347878736588
Mohammed Musharaf <mohammedm@zuper.local>
2024-03-04 08:14:50 +0530

    [feat] added Button Component

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,11 +1,24 @@
 import TitleSection from "@/components/landing-page/title-section";
+import { Button } from "@/components/ui";
 import React from "react";
 
 const HomePage = () => {
   return (
     <section>
       <div className="overflow-hidden px-4 sm:px-6 mt-10 sm:flex sm:flex-col gap-4 md:justify-center md:items-center">
-        <TitleSection pill="✨  Your Workspace, Perfected" title="All-In-One Collaboration and Productivity Platform"/>
+        <TitleSection
+          pill="✨  Your Workspace, Perfected"
+          title="All-In-One Collaboration and Productivity Platform"
+        />
+        <div className="bg-white p-[2px] mt-6 rounded-xl bg-gradient-to-r from-primary to-brand-primary-blue sm:w-[300px]">
+          <Button
+            variant={"btn-secondary"}
+            className="w-full rounded-[10px] p-6 text-2xl bg-background"
+          >
+            {" "}
+            Get Cypress Free
+          </Button>
+        </div>
       </div>
     </section>
   );
